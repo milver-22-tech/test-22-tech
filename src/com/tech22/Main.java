99bf7a347f26bca46f034a9fbdc21615f986bdfe
mflores22-Tech <milver.flores@22-tech.com>
2018-11-29 15:38:13 -0400

    test insert coins succesfull

--- src/com/tech22/Main.java
+++ src/com/tech22/Main.java
@@ -5,8 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		PuggyBank bank = new PuggyBank();
 		bank.setCoin(100);
+		bank.setCoin(100);
+		bank.setCoin(100);
+		bank.setCoin(100);
+		bank.setCoin(100);
 		bank.setCoin(200);
 		System.out.println(bank.getTotalCoins());
-		System.out.println(bank.getSpecificCoins(100));
+		System.out.println(bank.getSpecificCoins(200));
 	}
 }
